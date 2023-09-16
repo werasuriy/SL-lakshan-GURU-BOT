035eed42bfe5e1c38642080ec5b6acc1f921f1e5
werasuriy <145191033+werasuriy@users.noreply.github.com>
2023-09-16 14:19:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94740188210', 'me', true],
+  ['94740188210', 'SL lakshan', true], 
   [''] 
 ] //Number of owners
 
